c146a785435739f37aa017da9267fa0f9893b320
IanButcher <115805201+IanButcher@users.noreply.github.com>
2023-06-13 13:41:54 -0300

    Prueba

--- src/Js/app.js
+++ src/Js/app.js
@@ -1,5 +1,11 @@
+
 function LogInShow(logInMenu_var){
     logInMenu_var = document.getElementById("LogInMenu")
     onclick(logInMenu_var);
         alert("Mostrando menu de inicio de sesion") /* Despues hago que se muestre un menu bien hecho*/
+}
+function newNovedades(){
+    return Novedad;{
+        alert("Evento creado exitosamente");     
+    }
 }
