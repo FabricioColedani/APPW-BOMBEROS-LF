55568ec26a59df43009728ed37457a838bbe18a4
Fabricio Coledani <115805314+FabricioColedani@users.noreply.github.com>
2023-06-29 14:58:53 -0300

    Pages: INDEX Y LOGIN

--- APPWB-ANGULAR/src/app/pages/pages.module.ts
+++ APPWB-ANGULAR/src/app/pages/pages.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IndexComponent } from './index/index.component';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    IndexComponent,
+    LoginComponent
+  ],
   imports: [
     CommonModule
   ]
