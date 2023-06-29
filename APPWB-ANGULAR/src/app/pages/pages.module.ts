0de1e3a3e0325794a6eccf81922a9c0be9dbb193
Fabricio Coledani <115805314+FabricioColedani@users.noreply.github.com>
2023-06-29 15:04:16 -0300

    Pages: Profile

--- APPWB-ANGULAR/src/app/pages/pages.module.ts
+++ APPWB-ANGULAR/src/app/pages/pages.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IndexComponent } from './index/index.component';
 import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 
 @NgModule({
   declarations: [
     IndexComponent,
-    LoginComponent
+    LoginComponent,
+    ProfileComponent
   ],
   imports: [
     CommonModule
