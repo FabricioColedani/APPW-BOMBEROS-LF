fddb8c9468f8675dffb79ad5d73d779880dfed49
IanButcher <115805201+IanButcher@users.noreply.github.com>
2023-06-29 13:16:24 -0300

    Update pages.module.ts

--- AppBomberosAngular/src/app/pages/pages.module.ts
+++ AppBomberosAngular/src/app/pages/pages.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [
-    FooterComponent
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     CommonModule
