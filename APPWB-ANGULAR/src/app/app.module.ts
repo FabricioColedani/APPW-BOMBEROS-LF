bc1c5396f64d7715666d961e05f1ef8684b317f7
Fabricio Coledani <115805314+FabricioColedani@users.noreply.github.com>
2023-06-30 12:19:32 -0300

    ANGULAR FUNCIONANDO

--- APPWB-ANGULAR/src/app/app.module.ts
+++ APPWB-ANGULAR/src/app/app.module.ts
@@ -3,11 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule
